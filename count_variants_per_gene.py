756f2c7fd82124f0eae3d57bd01e3ccd21300293
Peter van Heusden <pvh@sanbi.ac.za>
2018-06-26 22:33:07 +0200

    Add argparse support

--- count_variants_per_gene.py
+++ count_variants_per_gene.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import argparse
+#from snptools.vc_matrix_async import make_variant_count_matrix
 from snptools.vc_matrix import make_variant_count_matrix
 
-input_directory = '/home/pvh/Data/vcf/combine_vcfs'
-output_filename = 'snp_matrix.csv'
-make_variant_count_matrix(input_directory, output_filename)
+parser = argparse.ArgumentParser('Count variants in genes and make a matrix')
+parser.add_argument('input_directory', default='/home/pvh/Data/vcf/combine_vcfs')
+parser.add_argument('output_filename', default='snp_matrix.csv')
+args = parser.parse_args()
+
+make_variant_count_matrix(args.input_directory, args.output_filename)
 
